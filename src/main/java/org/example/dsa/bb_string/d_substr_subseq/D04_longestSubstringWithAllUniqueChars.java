8e79ad7a85aa1173370616dab04163d5ab88bd07
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-11 19:52:48 +0530

    Refactor

--- src/main/java/org/example/dsa/bb_string/d_substr_subseq/D04_longestSubstringWithAllUniqueChars.java
+++ src/main/java/org/example/dsa/bb_string/d_substr_subseq/D04_longestSubstringWithAllUniqueChars.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 /**
  * *********************************************************************************
- * Find the longest substring With K unique characters
+ * Find the longest substring With All unique characters
  * *********************************************************************************
  */
 
