13b8ce98f4d4cc131f74211ad74e249e59ab6d15
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-20 19:59:26 +0530

    Stack problems

---
+++ src/main/java/org/example/dsa/zz1_stack/a_designstack/A08_designStack_usingPriorityQueue.java
@@ -0,0 +1,13 @@
+package org.example.dsa.zz1_stack.a_designstack;
+
+/**
+ * *****************************************************************************
+ * How to implement stack using priority queue or heap?
+ * *****************************************************************************
+ */
+
+public class A08_designStack_usingPriorityQueue {
+	/*
+	 * TODO
+	 */
+}
