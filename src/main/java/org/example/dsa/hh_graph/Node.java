4683ecb92cef2942254cb63efe072f40b02a88e9
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-25 03:01:09 +0530

    weighted DAG shortest path

---
+++ src/main/java/org/example/dsa/hh_graph/Node.java
@@ -0,0 +1,11 @@
+package org.example.dsa.hh_graph;
+
+public class Node {
+	public int val;
+	public int weight;
+	
+	public Node(int val, int weight) {
+		this.val = val;
+		this.weight = weight;
+	}
+}
---
+++ src/main/java/org/example/dsa/hh_graph/b_directed/B07_DAG_shortestPath.java
@@ -0,0 +1,72 @@
+package org.example.dsa.hh_graph.b_directed;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+import org.example.dsa.hh_graph.DirectedWeightedGraph;
+import org.example.dsa.hh_graph.Node;
+
+/**
+ * ***************************************************************************************
+ * Weighted DAG: Shortest path
+ * ***************************************************************************************
+ */
+
+public class B07_DAG_shortestPath {
+	
+	private static final int vertices = 6;
+	private static ArrayList<Node> adj[];
+	
+	
+	public static void main(String[] args) {
+		DirectedWeightedGraph graph = new DirectedWeightedGraph(vertices);
+		graph.addEdge(0,1, 2);
+		graph.addEdge(0,4, 1);
+		graph.addEdge(1,2, 3);
+		graph.addEdge(2,3, 6);
+		graph.addEdge(4,2, 2);
+		graph.addEdge(4,5, 4);
+		graph.addEdge(5,3, 1);
+		graph.printGraph();
+		adj = graph.adj;
+		
+		boolean visited[] = new boolean[vertices];
+		Stack<Integer> stack = new Stack<>();
+		dfs(0, visited, stack);
+		
+		int distance[] = new int[vertices];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		shortestPath(0, distance, stack);
+		System.out.println(Arrays.toString(distance));
+	}
+	
+	
+	private static void dfs(int current, boolean visited[], Stack<Integer> stack) {
+		visited[current] = true;
+		
+		for (Node adjNode : adj[current]) {
+			if (!visited[adjNode.val]) {				// if the adjacent node is not visited earlier
+				dfs(adjNode.val, visited, stack);
+			}
+		}
+		stack.push(current);							// at the end of each recursion call, push the current element in stack
+	}
+	
+	
+	private static void shortestPath(int current, int distance[], Stack<Integer> stack) {
+		distance[current] = 0;
+		
+		while (!stack.isEmpty()) {
+			current = stack.pop();
+			if (distance[current] != Integer.MAX_VALUE) {
+				for (Node adjNode : adj[current]) {
+					if (distance[current] + adjNode.weight < distance[adjNode.val]) {
+						distance[adjNode.val] = distance[current] + adjNode.weight;
+					}
+				}
+			}
+		}
+	}
+	
+}
