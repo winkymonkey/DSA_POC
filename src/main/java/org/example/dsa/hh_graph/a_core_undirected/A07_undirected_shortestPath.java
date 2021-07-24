b5f65e2cf98eb3451b6612e3e79bf26dd640fb7c
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-25 01:51:59 +0530

    undirected graph shortest path

---
+++ src/main/java/org/example/dsa/hh_graph/a_core_undirected/A07_undirected_shortestPath.java
@@ -0,0 +1,61 @@
+package org.example.dsa.hh_graph.a_core_undirected;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.example.dsa.hh_graph.UndirectedGraph;
+
+/**
+ * ***************************************************************************************
+ * Undirected Graph: find Shortest Path
+ * ***************************************************************************************
+ * 
+ *    1 -- 0 -- 3
+ *    |  /      |
+ *    | /       |
+ *    2         4
+ * 
+ * ***************************************************************************************
+ */
+
+public class A07_undirected_shortestPath {
+	
+	private static final int vertices = 5;
+	private static ArrayList<Integer> adj[];
+	
+	
+	public static void main(String[] args) {
+		UndirectedGraph graph = new UndirectedGraph(vertices);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(2, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(3, 4);
+		graph.printGraph();
+		adj = graph.adj;
+		
+		int distance[] = new int[vertices];		Arrays.fill(distance, Integer.MAX_VALUE);
+		bfs(0, distance);
+		System.out.println(Arrays.toString(distance));
+	}
+	
+	
+	private static void bfs(int current, int distance[]) {
+		Queue<Integer> queue = new LinkedBlockingQueue<>();
+		queue.add(current);
+		distance[current] = 0;
+		
+		while (!queue.isEmpty()) {
+			current = queue.poll();
+			for (int adjNode : adj[current]) {
+				if (distance[current]+1 < distance[adjNode]) {
+					queue.add(adjNode);
+					distance[adjNode] = distance[current]+1;
+				}
+			}
+		}
+	}
+	
+}
