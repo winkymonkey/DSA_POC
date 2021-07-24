691c5026d08a694f5f9d9c6e2ad4083cb93dbe03
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-25 02:30:54 +0530

    rewrite in a new way

---
+++ src/main/java/org/example/dsa/hh_graph/Pair.java
@@ -0,0 +1,11 @@
+package org.example.dsa.hh_graph;
+
+public class Pair {
+	public int current;
+	public int parent;
+	
+	public Pair(int current, int parent) {
+		this.current = current;
+		this.parent = parent;
+	}
+}
--- src/main/java/org/example/dsa/hh_graph/a_core_undirected/A04_undirected_detectCycle_BFS.java
+++ src/main/java/org/example/dsa/hh_graph/a_core_undirected/A04_undirected_detectCycle_BFS.java
@@ -1,10 +1,10 @@
 package org.example.dsa.hh_graph.a_core_undirected;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.example.dsa.hh_graph.Pair;
 import org.example.dsa.hh_graph.UndirectedGraph;
 
 /**
@@ -37,27 +37,25 @@ public class A04_undirected_detectCycle_BFS {
 		adj = graph.adj;
 		
 		boolean visited[] = new boolean[vertices];
-		int parent[] = new int[vertices];			Arrays.fill(parent, -1);		// stores parent of each node
-		System.out.println(bfs(0, visited, parent));
+		System.out.println(bfs(new Pair(0, -1), visited));
 	}
 	
 	
 	// detect cycle using BFS
-	private static boolean bfs(int current, boolean visited[], int parent[]) {
-		Queue<Integer> queue = new LinkedBlockingQueue<>();
-		queue.add(current);
-		visited[current] = true;
+	private static boolean bfs(Pair pair, boolean visited[]) {
+		Queue<Pair> queue = new LinkedBlockingQueue<>();
+		queue.add(pair);
+		visited[pair.current] = true;
 
 		while (queue.size() != 0) {
-			current = queue.poll();
+			pair = queue.poll();
 
-			for (int adjNode : adj[current]) {
+			for (int adjNode : adj[pair.current]) {
 				if (!visited[adjNode]) {					// if the adjacent node is not visited earlier
-					queue.add(adjNode);
+					queue.add(new Pair(adjNode, pair.current));
 					visited[adjNode] = true;
-					parent[adjNode] = current;
 				}
-				else if (adjNode != parent[current]) {		// if the adjacent node is already visited but it's not the parent node, then it's a cycle
+				else if (adjNode != pair.parent) {			// if the adjacent node is already visited but it's not the parent node, then it's a cycle
 					return true;
 				}
 			}
