c8489207bad9508c12bf5a4a58c977442d44b850
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-24 19:55:14 +0530

    rename

--- src/main/java/org/example/dsa/hh_graph/a_core/A09_directed_detectCycle_DFS.java
+++ src/main/java/org/example/dsa/hh_graph/a_core/A09_directed_detectCycle_DFS.java
@@ -10,7 +10,7 @@ import org.example.dsa.hh_graph.DirectedGraph;
  * ***************************************************************************************
  */
 
-public class A06_directed_detectCycle_DFS {
+public class A09_directed_detectCycle_DFS {
 	
 	private static final int vertices = 4;
 	private static ArrayList<Integer> adj[];
