6073f8829ce212bb3e53dcd7ee9401bb1166f94e
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-25 20:58:13 +0530

    a few problems

---
+++ src/main/java/org/example/dsa/aa_array/q_matrix/Q01_setMartixZeros.java
@@ -0,0 +1,51 @@
+package org.example.dsa.aa_array.q_matrix;
+
+/**
+ * *****************************************************************************
+ * Given a m*n matrix if an element is 0, set its entire row & column to 0
+ * *****************************************************************************
+ * Input:					Output:
+ *    [[1,1,1]					[[1,0,1]
+ *     [1,0,1]					 [0,0,0]
+ *     [1,1,1]]					 [1,0,1]]
+ * 
+ * Input:					Output:
+ *    [[0,1,2,0]				[[0,0,0,0]
+ *     [3,4,5,2]				 [0,4,5,0]
+ *     [1,3,1,5]]				 [0,3,1,0]]
+ * 
+ * *****************************************************************************
+ */
+
+public class Q01_setMartixZeros {
+	/*
+	 * --------------------
+	 * ---NAIVE APPROACH---
+	 * --------------------
+	 * Traverse the matrix.
+	 * Whenever we find a 0, we start traversing its row & columns and make every non-zero element to -1
+	 * 
+	 * Traverse the matrix again to convert all these -1 to 0.
+	 * 
+	 * TIME --- O(n*m) * O(n+m)
+	 * SPACE -- O(1)
+	 * 
+	 * 
+	 * 
+	 * ------------------------
+	 * ---EFFICIENT APPROACH---
+	 * ------------------------
+	 * Maintain an array to represent the matrix column
+	 * Maintain an array to represent the matrix row
+	 * 
+	 * Traverse the matrix.
+	 * Whenever we find a 0, make corresponding cell in row-array as 0 and column-array as 0
+	 * 
+	 * Traverse the matrix again.
+	 * for each cell, if either of its corresponding row-array or column-array contains 0, convert the matrix cell as 0
+	 * 
+	 * TIME --- O(n*m)
+	 * SPACE -- O(1)
+	 * 
+	 */
+}
