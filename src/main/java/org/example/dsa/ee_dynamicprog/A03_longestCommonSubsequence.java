4680e4b1deb2a7715379b7abecea64e2d4dcb612
winkymonkey <pal.abhishek.2008@gmail.com>
2021-09-11 23:25:46 +0530

    new problems

--- src/main/java/org/example/dsa/ee_dynamicprog/A03_longestCommonSubsequence.java
+++ src/main/java/org/example/dsa/ee_dynamicprog/A03_longestCommonSubsequence.java
@@ -1,5 +1,7 @@
 package org.example.dsa.ee_dynamicprog;
 
+import java.util.Arrays;
+
 /**
  * ***************************************************************************************
  * Longest Common Subsequence
@@ -62,6 +64,59 @@ public class A03_longestCommonSubsequence {
 	 * 		- now we can keep str2 intact & ignore last character of str1. And then we can proceed.
 	 * 		- between these two whichever brings the maximum length, we pick that because our objective is to find the subsequence that is longest.
 	 * 
+	 * --------------
+	 * Recursion Tree
+	 * (a few branches are omitted for brevity)
+	 * --------------
+	 *                                                                F(AGGTAB,GXTXAYB)
+	 *                                                                        |
+	 *                                                                        |
+	 *                                                             1 + F(AGGTA,GXTXAY)
+	 *                                                             ___________|___________
+	 *                                                            |                       |
+	 *                                                     F(AGGTA,GXTXA)          F(AGGT,GXTXAY)
+	 *                                                            |                  _____|_____
+	 *                                                            |                 |           |
+	 *                                                    1+ F(AGGT,GXTX)
+	 *                             _______________________________|______________________________
+	 *                            |                                                              |
+	 *                       F(AGGT,GXT)                                                    F(AGG,GXTX)
+	 *                            |                                                ______________|______________
+	 *                            |                                               |                             |
+	 *                      1+ F(AGG,GX)                                     F(AGG,GXT)                    F(AG,GXTX)
+	 *                  __________|_________                                 _____|________________        _____|_____
+	 *                 |                    |                               |                      |      |           |
+	 *              F(AGG,G)             F(AG,GX)                       F(AGG,GX)              F(AG,GXT)
+	 *             ____|____           ______|_____                  _______|_______          _____|_____
+	 *            |         |         |            |                |               |        |           |
+	 *        F(AGG,'')  F(AG,G)   F(AG,G)      F(A,GX)          F(AGG,G)       F(AG,GX)
+	 *                      |         |        ____|____            |           _____|_____
+	 *                      |         |       |         |           |          |           |
+	 *                1+ F(A,'')   F(A,'')  F(A,G)  F('',GX)   1+ F(AG,'')  F(AG,G)      F(A,GX)
+	 *                                     ___|___                             |         ___|___
+	 *                                    |       |                            |        |       |
+	 *                                 F(A,'')  F('',G)                   1+ F(A,'')  F(A,G) F('',GX)
+	 *                                                                               ___|___
+	 *                                                                              |       |
+	 *                                                                           F(A,'')  F('',G)
+	 * 
+	 * TIME --- O(2^n)
+	 * SPACE -- O(1)
+	 * 
+	 * 
+	 * 
+	 * -------------------------
+	 * ---DYNAMIC PROGRAMMING---
+	 * -------------------------
+	 * In the above recursion tree, there are many function calls which are computed repeatedly
+	 * For example, F(AG,GX)
+	 * 
+	 * So we can apply dynamic programming here.
+	 * We will apply Top-Down (memoization) approach here.
+	 * 
+	 * TIME --- O(mn)
+	 * SPACE -- O(mn)
+	 * 
 	 */
 	
 	public static void main(String[] args) {
@@ -72,22 +127,57 @@ public class A03_longestCommonSubsequence {
 		char arr2[] = s2.toCharArray();
 		int len1 = arr1.length;
 		int len2 = arr2.length;
-
-		System.out.println(findLcs(arr1, arr2, len1, len2));
+		
+		int subseqLength = findLcs(arr1, arr2, len1, len2);
+		System.out.println(subseqLength);
+		
+		int subseqLength2 = findLcs_dynamic(arr1, arr2, len1, len2);
+		System.out.println(subseqLength2);
 	}
 	
 	
 	private static int findLcs(char arr1[], char arr2[], int len1, int len2) {
-		if (len1 == 0 || len2 == 0) {
+		if (len1 == 0 || len2 == 0)
 			return 0;
-		}
+		
 		if (arr1[len1-1] == arr2[len2-1]) {						// If last characters of both the strings are same
 			return 1 + findLcs(arr1, arr2, len1-1, len2-1);		// Ignore last characters of both the strings & recur for remaining strings of lengths m-1 and n-1
 		}
 		else {
 			int length1 = findLcs(arr1, arr2, len1, len2-1);	// find length of the longest subsequence while ignoring the last character of str1
 			int length2 = findLcs(arr1, arr2, len1-1, len2);	// find length of the longest subsequence while ignoring the last character of str2
-			return Math.max(length1, length2);					// pick the longest among these two
+			return Math.max(length1, length2);					// pick the longest among these two because we are asked to find out the Longest Common Subsequence
+		}
+	}
+	
+	
+	
+	
+	
+	private static final int lookup[][];
+	static {
+		lookup = new int[1000][1000];							// should be of size "len1+1" * "len2+1"
+		for (int i=0; i<lookup.length; i++) {
+			Arrays.fill(lookup[i], -1);
+		}
+	}
+	
+	private static int findLcs_dynamic(char arr1[], char arr2[], int len1, int len2) {
+		if (len1 == 0 || len2 == 0)
+			return 0;
+		
+		if (lookup[len1-1][len2-1] != -1)
+			return lookup[len1][len2];
+		
+		if (arr1[len1-1] == arr2[len2-1]) {										// If last characters of both the strings are same
+			lookup[len1-1][len2-1] = 1 + findLcs(arr1, arr2, len1-1, len2-1);	// Ignore last characters of both the strings & recur for remaining strings of lengths m-1 and n-1
+			return lookup[len1-1][len2-1];
+		}
+		else {
+			int length1 = findLcs(arr1, arr2, len1, len2-1);					// find length of the longest subsequence while ignoring the last character of str1
+			int length2 = findLcs(arr1, arr2, len1-1, len2);					// find length of the longest subsequence while ignoring the last character of str2
+			lookup[len1-1][len2-1] = Math.max(length1, length2);				// pick the longest among these two because we are asked to find out the Longest Common Subsequence
+			return lookup[len1-1][len2-1];
 		}
 	}
 	
--- src/main/java/org/example/dsa/ee_dynamicprog/A04_longestRecurringSubsequence.java
+++ src/main/java/org/example/dsa/ee_dynamicprog/A04_longestRecurringSubsequence.java
@@ -1,5 +1,71 @@
 package org.example.dsa.ee_dynamicprog;
 
-public class A04_longestRecurringSubsequence {
+/**
+ * ***************************************************************************************
+ * Longest Repeating Subsequence
+ * ***************************************************************************************
+ * Given a string, find the length of the longest repeating subsequence such that the two subsequences don’t have same string character at the same position,
+ * i.e., any i’th character in the two subsequences shouldn’t have the same index in the original string.
+ * ***************************************************************************************
+ */
 
+public class A04_longestRecurringSubsequence {
+	/*
+	 * ------------------------
+	 * ---EFFICIENT APPROACH---
+	 * ------------------------
+	 * This problem is just a modification of LCS problem.
+	 * Here we need to find LCS of two same strings, but under some restriction.
+	 * The restriction is i-th index in one string cannot be equals to the j-th index of the second string.
+	 * 
+	 * So the below logic should be updated:
+	 * 		if (arr1[len1-1] == arr2[len2-1])
+	 * 
+	 * The updated logic should be:
+	 * 		if (arr1[len1-1] == arr2[len2-1] && len1!=len2)
+	 * 
+	 * 
+	 * TIME --- O(2^n)
+	 * SPACE -- O(1)
+	 * 
+	 * 
+	 * 
+	 * -------------------------
+	 * ---DYNAMIC PROGRAMMING---
+	 * -------------------------
+	 * Like LCS, this problem can also be extended in dynamic programming which will bring down the time complexity.
+	 * 
+	 * TIME --- O(mn)
+	 * SPACE -- O(mn)
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		String s1 = "AGGTAB";
+		String s2 = "GXTXAYB";
+
+		char arr1[] = s1.toCharArray();
+		char arr2[] = s2.toCharArray();
+		int len1 = arr1.length;
+		int len2 = arr2.length;
+		
+		int subseqLength = findLrs(arr1, arr2, len1, len2);
+		System.out.println(subseqLength);
+	}
+	
+	
+	private static int findLrs(char arr1[], char arr2[], int len1, int len2) {
+		if (len1 == 0 || len2 == 0)
+			return 0;
+		
+		if (arr1[len1-1] == arr2[len2-1] && len1!=len2) {		// If last characters of both the strings are same, but both have different lengths
+			return 1 + findLrs(arr1, arr2, len1-1, len2-1);		// Ignore last characters of both the strings & recur for remaining strings of lengths m-1 and n-1
+		}
+		else {
+			int length1 = findLrs(arr1, arr2, len1, len2-1);	// find length of the longest subsequence while ignoring the last character of str1
+			int length2 = findLrs(arr1, arr2, len1-1, len2);	// find length of the longest subsequence while ignoring the last character of str2
+			return Math.max(length1, length2);					// pick the longest among these two because we are asked to find out the Longest Common Subsequence
+		}
+	}
+	
 }
